29db8cecf8219fd210095f650589936f9ca5bc4d
luisrguerra <luiseduardoribeiroguerra@gmail.com>
2022-11-15 17:41:34 -0300

    atualizar

--- fluxo/js/saldo.js
+++ fluxo/js/saldo.js
@@ -1,3 +1,26 @@
+function addStyle(styleString) {
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
+};
+
+addStyle(`
+.saldoNegativo{
+    color:#d93025 !important;
+} 
+.saldoNegativo::selection {
+    color: white;
+    background: #d93025;
+}
+.saldoPositivo{
+    color:#188038 !important;
+}
+.saldoPositivo::selection {
+    color: white;
+    background: #188038;
+}
+`);
+
 const allSaldoByClass = document.getElementsByClassName('saldo');
 for (let contagem = 0; contagem < allSaldoByClass.length; contagem++) {
   const element =  allSaldoByClass[contagem];
@@ -9,4 +32,4 @@ for (let contagem = 0; contagem < allSaldoByClass.length; contagem++) {
   else{ /*positivo*/
    element.classList.add("saldoPositivo");
   }
-}
+};
